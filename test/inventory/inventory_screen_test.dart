cd1d4034857ab05d2fc49f9a192b4dd50e2eafd3
TangoSplicer <202358028+TangoSplicer@users.noreply.github.com>
2026-05-06 14:59:26 -0400

    Fix inventory screen test dependencies and analytics service missing import

--- test/inventory/inventory_screen_test.dart
+++ test/inventory/inventory_screen_test.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
 import 'package:artisanarc/features/inventory/presentation/inventory_screen.dart';
+import 'package:artisanarc/features/inventory/domain/inventory_service.dart';
 
+import 'inventory_screen_test.mocks.dart';
+
+@GenerateMocks([InventoryService])
 void main() {
+  late MockInventoryService mockService;
+
+  setUp(() {
+    mockService = MockInventoryService();
+    final getIt = GetIt.instance;
+    getIt.reset();
+    getIt.registerSingleton<InventoryService>(mockService);
+
+    when(mockService.fetchItems()).thenAnswer((_) async => []);
+  });
+
   testWidgets('InventoryScreen shows empty message and FAB', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: InventoryScreen()));
+    await tester.pumpAndSettle();
 
-    expect(find.text('No items found. Tap + to add.'), findsOneWidget);
-    expect(find.byIcon(Icons.add), findsOneWidget);
+    expect(find.text('No Items Yet'), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsWidgets); // FAB and Action button both have Icons.add
   });
 }
